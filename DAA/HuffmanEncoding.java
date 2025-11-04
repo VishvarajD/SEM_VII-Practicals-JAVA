7ff393b5f9216630cee336e08a49fb5dbba9d708
Vishvaraj18 <vishvaraj.dgkgr22@sinhgad.edu>
2025-11-04 13:30:26 +0530

    Huffman encoding using Min Heap

---
+++ DAA/HuffmanEncoding.java
@@ -0,0 +1,56 @@
+import java.util.PriorityQueue;
+
+public class HuffmanEncoding {
+
+    static class Node implements Comparable<Node> {
+        char ch;
+        int freq;
+        Node left, right;
+
+        Node(char ch, int freq) {
+            this.ch = ch;
+            this.freq = freq;
+        }
+
+        // Compare nodes based on frequency
+        public int compareTo(Node other) {
+            return this.freq - other.freq;
+        }
+    }
+
+    // Recursive function to print codes
+    static void printNodes(Node root, String code){
+        if(root.left==null && root.right==null){
+            System.out.println(root.ch + ": " + code);
+            return;
+        }
+        printNodes(root.left,code + '0');
+        printNodes(root.right,code + '1');
+    }
+    
+
+    public static void main(String[] args) {
+        char[] chars = { 'a', 'b', 'c', 'd', 'e', 'f' };
+        int[] freq = { 5, 9, 12, 13, 16, 45 };
+
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+       for(int i = 0; i<chars.length;i++){
+        pq.add(new Node(chars[i],freq[i]));
+       }
+
+       while(pq.size() > 1){
+        Node left = pq.poll();
+        Node right = pq.poll();
+        Node parent = new Node('-',left.freq + right.freq);
+        parent.left = left;
+        parent.right = right;
+        pq.add(parent);
+       }
+       printNodes(pq.peek(),"");
+    }
+}
+//time Complexity per operation => O(logn)
+//Total time Complexity => O(nlogn)
+//space Complexity =>  O(n) priority Queue
